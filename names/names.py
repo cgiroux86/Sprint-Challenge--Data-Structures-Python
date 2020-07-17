c86bba242198f186bf117401c5b43193210d1e45
Chris Giroux <cgiroux86@gmail.com>
2020-07-17 13:16:44 -0400

    commented out dbly linked list solution

--- names/names.py
+++ names/names.py
@@ -20,7 +20,6 @@ idx = s.pop(math.floor(len(s) / 2))
 root = BSTNode(idx)
 
 duplicates = []  # Return the list of duplicates in this data structure
-d = []
 
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
@@ -41,32 +40,3 @@ print(f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
-
-# from bst import BSTNode
-
-# start_time = time.time()
-
-# f = open('names_1.txt', 'r')
-# names_1 = f.read().split("\n")  # List containing 10000 names
-# f.close()
-
-# f = open('names_2.txt', 'r')
-# names_2 = f.read().split("\n")  # List containing 10000 names
-# f.close()
-
-# duplicates = []  # Return the list of duplicates in this data structure
-
-# # Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-
-# end_time = time.time()
-# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print (f"runtime: {end_time - start_time} seconds")
-
-# # ---------- Stretch Goal -----------
-# # Python has built-in tools that allow for a very efficient approach to this problem
-# # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# # structures, but you may not import any additional libraries that you did not write yourself.
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,101 +1,101 @@
-# class RingBuffer:
-#     def __init__(self, capacity):
-#         self.capacity = capacity
-#         self.data = []
-#         self.tracker = 0
-
-#     def append(self, item):
-#         if len(self.data) < self.capacity:
-#             self.data.append(item)
-#         else:
-#             if self.tracker == len(self.data) - 1:
-#                 self.data[self.tracker] = item
-#                 self.tracker = 0
-
-#             else:
-#                 self.data[self.tracker] = item
-#                 self.tracker += 1
-
-#     def get(self):
-#         lst = []
-#         for d in self.data:
-#             lst.append(d)
-#         return lst
-class Node:
-    def __init__(self, value):
-        self.next = None
-        self.prev = None
-        self.value = value
-
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.head = None
-        self.tail = None
-        self.size = 0
-        self.tracker = None
+        self.data = []
+        self.tracker = 0
 
     def append(self, item):
-        new_node = Node(item)
-        if self.size < self.capacity:
-            if not self.head:
-                self.head = new_node
-                self.tail = new_node
-                self.tracker = new_node
-                self.size += 1
-            else:
-                if self.head is self.tail:
-                    self.head.next = new_node
-                    new_node.prev = self.head
-                    self.tail = new_node
-                self.tail.next = new_node
-                new_node.prev = self.tail
-                self.tail = new_node
-                self.size += 1
+        if len(self.data) < self.capacity:
+            self.data.append(item)
         else:
-            if self.tracker is self.head:
-                self.tracker = self.tracker.next
-                new_node.next = self.head.next
-                self.head.next.prev = new_node
-                self.head = new_node
-            elif self.tracker is self.tail:
-                self.tail.prev.next = new_node
-                new_node.prev = self.tail.prev
-                self.tail = new_node
-                self.tracker = self.head
-            else:
-                curr = self.head
-                while curr and curr != self.tracker:
-                    curr = curr.next
-                if curr.prev is self.head:
-                    self.tracker = curr.next
-                    self.head.next = new_node
-                    new_node.prev = self.head
-                    new_node.next = curr.next
-                    curr = new_node
-                curr.prev.next = new_node
-                curr.next.prev = new_node
-                new_node.prev = curr.prev
-                new_node.next = curr.next
-                curr = new_node
-                self.tracker = curr.next
+            if self.tracker == len(self.data) - 1:
+                self.data[self.tracker] = item
+                self.tracker = 0
 
-    def print_list(self):
-        curr = self.head
-        lst = []
-        while curr:
-            lst.append(curr.value)
-            curr = curr.next
-        print(lst)
+            else:
+                self.data[self.tracker] = item
+                self.tracker += 1
 
     def get(self):
-        curr = self.head
         lst = []
-        while curr:
-            lst.append(curr.value)
-            curr = curr.next
+        for d in self.data:
+            lst.append(d)
         return lst
+# class Node:
+#     def __init__(self, value):
+#         self.next = None
+#         self.prev = None
+#         self.value = value
+
+
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.head = None
+#         self.tail = None
+#         self.size = 0
+#         self.tracker = None
+
+#     def append(self, item):
+#         new_node = Node(item)
+#         if self.size < self.capacity:
+#             if not self.head:
+#                 self.head = new_node
+#                 self.tail = new_node
+#                 self.tracker = new_node
+#                 self.size += 1
+#             else:
+#                 if self.head is self.tail:
+#                     self.head.next = new_node
+#                     new_node.prev = self.head
+#                     self.tail = new_node
+#                 self.tail.next = new_node
+#                 new_node.prev = self.tail
+#                 self.tail = new_node
+#                 self.size += 1
+#         else:
+#             if self.tracker is self.head:
+#                 self.tracker = self.tracker.next
+#                 new_node.next = self.head.next
+#                 self.head.next.prev = new_node
+#                 self.head = new_node
+#             elif self.tracker is self.tail:
+#                 self.tail.prev.next = new_node
+#                 new_node.prev = self.tail.prev
+#                 self.tail = new_node
+#                 self.tracker = self.head
+#             else:
+#                 curr = self.head
+#                 while curr and curr != self.tracker:
+#                     curr = curr.next
+#                 if curr.prev is self.head:
+#                     self.tracker = curr.next
+#                     self.head.next = new_node
+#                     new_node.prev = self.head
+#                     new_node.next = curr.next
+#                     curr = new_node
+#                 curr.prev.next = new_node
+#                 curr.next.prev = new_node
+#                 new_node.prev = curr.prev
+#                 new_node.next = curr.next
+#                 curr = new_node
+#                 self.tracker = curr.next
+
+#     def print_list(self):
+#         curr = self.head
+#         lst = []
+#         while curr:
+#             lst.append(curr.value)
+#             curr = curr.next
+#         print(lst)
+
+#     def get(self):
+#         curr = self.head
+#         lst = []
+#         while curr:
+#             lst.append(curr.value)
+#             curr = curr.next
+#         return lst
 
 
 a = RingBuffer(5)
